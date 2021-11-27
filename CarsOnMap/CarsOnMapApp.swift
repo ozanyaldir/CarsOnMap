c72a55eb9aa317ce938992a683b5e7c1371035b9
Ozan Yaldır <ozan@arf.one>
2021-11-27 16:40:15 +0300

    Initial Commit

---
+++ CarsOnMap/CarsOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsOnMapApp.swift
+//  CarsOnMap
+//
+//  Created by Ozan YALDIR on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CarsOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
