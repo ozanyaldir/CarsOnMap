7fe3d6dcd336d89b1b034053979f50cbcd887164
Ozan Yaldır <ozan@arf.one>
2021-11-27 17:22:01 +0300

    AppDelegate is initialized

---
+++ CarsOnMap/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CarsOnMap
+//
+//  Created by Ozan YALDIR on 27.11.2021.
+//
+
+import Foundation
+import UIKit
+//import netfox
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Your code here")
+//        NFX.sharedInstance().start()
+        return true
+    }
+}
