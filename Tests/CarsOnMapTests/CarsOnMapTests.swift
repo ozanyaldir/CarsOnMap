b73f6cd2a6c74ba52ac79667be62da9e3d85fb7b
Ozan Yaldır <ozan@arf.one>
2021-12-06 02:19:29 +0300

    Package Manager is initialized

---
+++ Tests/CarsOnMapTests/CarsOnMapTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CarsOnMap
+
+final class CarsOnMapTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CarsOnMap().text, "Hello, World!")
+    }
+}
